e5854a5ee0f27e837b275f0eec86574b1b214e2f
o young <rladhdud2@gmail.com>
2023-02-14 09:46:46 +0900

    Update StompWebSocketConfig.java

--- bogeum2/src/main/java/com/cos/bogeum/config/StompWebSocketConfig.java
+++ bogeum2/src/main/java/com/cos/bogeum/config/StompWebSocketConfig.java
@@ -1,9 +1,10 @@
 package com.cos.bogeum.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
  * @ServerEndpoint 어노테이션이 달린 클래스들은 WebSocket이 생성될 때마다
